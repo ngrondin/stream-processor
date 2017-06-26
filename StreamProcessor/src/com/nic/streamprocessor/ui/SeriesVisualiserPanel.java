d71571d86cad227aacb5953d556c43eeb2e17ab8
NGrondi <NGrondi@VNTG6350VS4.corp.ventia.com.au>
2017-06-26 22:18:39 +1000

    contribution

--- StreamProcessor/src/com/nic/streamprocessor/ui/SeriesVisualiserPanel.java
+++ StreamProcessor/src/com/nic/streamprocessor/ui/SeriesVisualiserPanel.java
@@ -2,34 +2,78 @@ package com.nic.streamprocessor.ui;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
-import java.awt.geom.Line2D;
-import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
 import com.nic.streamprocessor.streamtargets.MemoryStreamTarget;
 
+
 @SuppressWarnings("serial")
-public class SeriesVisualiserPanel extends JPanel implements MouseWheelListener
+public class SeriesVisualiserPanel extends JPanel implements MouseWheelListener, MouseListener
 {
 	protected MemoryStreamTarget target;
 	protected float xScale;
 	protected float yScale;
 	protected int start;
 	
+	protected Color[] colorArray = new Color[]{Color.BLUE, Color.RED, Color.ORANGE, Color.MAGENTA, Color.BLACK, Color.CYAN};
+	protected ArrayList<ClickEvent> clickEvents;
+	protected long activeChannelMask;
+	
+	class ClickEvent
+	{
+		public int x;
+		public int y;
+		public int r;
+		public String function;
+		public int param;
+		
+		public ClickEvent(int x1, int y1, int r1, String f, int p)
+		{
+			x = x1;
+			y = y1;
+			r = r1;
+			function = f;
+			param = p;
+		}
+	}
 	
 	public SeriesVisualiserPanel(MemoryStreamTarget t)
 	{
-		target = t;
 		xScale = 0.5f;
 		yScale = 5f;
 		start = 0;
+		clickEvents = new ArrayList<ClickEvent>();
 		addMouseWheelListener(this);
+		addMouseListener(this);
+		setTarget(t);
+	}
+	
+	public void setTarget(MemoryStreamTarget t)
+	{
+		target = t;
+		activeChannelMask = (1 << (target.getChannels().length + 1)) - 1;
+		float maxAbsY = 0f;
+		for(int c = 0; c < target.getChannels().length; c++)
+		{
+			String channel = target.getChannels()[c];
+			ArrayList<Float> serie = target.getSeries(channel);
+			for(int i = 0; i < serie.size(); i++)
+				if(Math.abs(serie.get(i)) > maxAbsY)
+					maxAbsY = Math.abs(serie.get(i));
+		}
+		int height = getHeight();
+		if(height == 0)
+			height = 400;
+		yScale = ((float)height / 2f) / maxAbsY;
 	}
 	
 	public void xZoom(int xz)
@@ -52,6 +96,12 @@ public class SeriesVisualiserPanel extends JPanel implements MouseWheelListener
 		repaint();
 	}
 	
+	public void toggleChannel(int c)
+	{
+		activeChannelMask ^= (1 << c);
+		repaint();
+	}
+	
 	
 	public void paintComponent(Graphics g) 
 	{
@@ -65,40 +115,53 @@ public class SeriesVisualiserPanel extends JPanel implements MouseWheelListener
 		float x = 0f;
 		float lastx = 0f;
 		int lasty = zeroLine;
-		Color color = null;
+	
+		clickEvents.clear();
 		
 		for(int c = 0; c < channels.length; c++)
 		{
 			ArrayList<Float> serie = target.getSeries(channels[c]);
-			if(c == 0) color = Color.blue;
-			if(c == 1) color = Color.RED;
-			if(c == 2) color = Color.GREEN;
-			if(c == 3) color = Color.YELLOW;
-			g.setColor(color);
-			i = (float)start;
-			x = 0f;
-			lastx = 0f;
-			lasty = zeroLine;
-			
-			while(x < width &&  i < serie.size())
+			g.setColor(colorArray[c % 6]);
+			int labelHeight = height - 20 *(target.getChannels().length - c + 1);
+			g2.drawString(channels[c], 20, labelHeight);
+			clickEvents.add(new ClickEvent(14, labelHeight - 4, 8, "channel", c));
+
+			if((activeChannelMask & (1 << c)) > 0)
 			{
-				int y = zeroLine - (int)(yScale * target.getSeries(channels[c]).get((int)i));
-				g2.drawLine((int)lastx, lasty, (int)x, y);
-				lastx = x;
-				lasty = y;
-				if(xScale >= 1f)
+				i = (float)start;
+				x = 0f;
+				lastx = 0f;
+				lasty = zeroLine;
+				
+				while(x < width &&  i < serie.size())
 				{
-					x += xScale;
-					i += 1f;
+					int y = zeroLine - (int)(yScale * target.getSeries(channels[c]).get((int)i));
+					g2.drawLine((int)lastx, lasty, (int)x, y);
+					lastx = x;
+					lasty = y;
+					if(xScale >= 1f)
+					{
+						x += xScale;
+						i += 1f;
+					}
+					else
+					{
+						x += 1f;
+						i += 1f / xScale;
+					}
 				}
-				else
-				{
-					x += 1f;
-					i += 1f / xScale;
-				}
+				g2.fillArc(10, labelHeight - 8, 8, 8, 0, 360);
 			}
-			
 		}
+		
+		g2.setColor(Color.GRAY);
+		g2.setFont(new Font("Arial", Font.BOLD, 20));
+		g2.drawString("+", width - 30, height - 50);
+		g2.drawArc(width - 34, height - 68, 20, 20, 0, 360);
+		clickEvents.add(new ClickEvent(width - 30, height - 65, 10, "zoom", 10));
+		g2.drawString("-", width - 28, height - 30);
+		g2.drawArc(width - 34, height - 46, 20, 20, 0, 360);
+		clickEvents.add(new ClickEvent(width - 30, height - 45, 10, "zoom", -10));
 	}
 	
 	public void mouseWheelMoved(MouseWheelEvent e)
@@ -112,5 +175,46 @@ public class SeriesVisualiserPanel extends JPanel implements MouseWheelListener
 			yZoom(-amt);
 		else
 			scroll(10*amt);		
+	}
+
+	public void mouseClicked(MouseEvent arg0) 
+	{
+		for(int i = 0; i < clickEvents.size(); i++)
+		{
+			int dx = clickEvents.get(i).x - arg0.getX();
+			int dy = clickEvents.get(i).y - arg0.getY();
+			int d = (int)Math.sqrt((dx*dx)+(dy*dy));
+			if(d < clickEvents.get(i).r)
+			{
+				if(clickEvents.get(i).function.equals("zoom"))
+				{
+					xZoom(clickEvents.get(i).param);
+				}
+				else if(clickEvents.get(i).function.equals("channel"))
+				{
+					toggleChannel(clickEvents.get(i).param);
+				}
+			}
+		}
+	}
+
+	public void mouseEntered(MouseEvent arg0) 
+	{
+		
+	}
+
+	public void mouseExited(MouseEvent arg0) 
+	{
+		
+	}
+
+	public void mousePressed(MouseEvent arg0) 
+	{
+		
+	}
+
+	public void mouseReleased(MouseEvent arg0) 
+	{
+		
 	}
 }
